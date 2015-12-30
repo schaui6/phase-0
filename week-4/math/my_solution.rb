29adbaec1af2a3bd27ad360341d4cc9184a4eb96
schaui6 <sanderfer.chau@gmail.com>
2015-12-29 19:39:41 -0800

    4.3.2

--- week-4/math/my_solution.rb
+++ week-4/math/my_solution.rb
@@ -6,18 +6,21 @@
 # Your Solution Below
 
 def add(num_1, num_2)
+ #your code here
  num_1 + num_2
 end
 
 def subtract(num_1, num_2)
+ #your code here
  num_1 - num_2
 end
 
 def multiply(num_1, num_2)
+ #your code here
  num_1 * num_2
 end
 
 def divide(num_1, num_2)
+ #your code here
  num_1 / num_2
 end
-
