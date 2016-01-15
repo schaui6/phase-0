4c56feb8352afbbdcc3f491b3c1910753c2112e3
schaui6 <sanderfer.chau@gmail.com>
2016-01-14 18:44:56 -0800

    6.5

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -59,3 +59,20 @@ p number_array
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+
+=begin
+
+What are some general rules you can apply to nested arrays?
+
+Think of nested arrays as a sub-array, and think in terms of one array at a time to avoid confusion.  As you access the array, access only one layer at a time to see where you are really targeting.
+
+What are some ways you can iterate over nested arrays?
+
+You could iterate to sort the array to find nested arrays with the kind_of? method.
+
+Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+The kind_of? method was new to me and great for sorting out subarrays so I can iterate over the subarray.  It allowed me to iterate the array to find the object I wanted which was for another array(sub arrays) so that I could iterate through that sub array.
+
+=end
