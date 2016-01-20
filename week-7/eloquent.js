0f569fdd690267e7190b2e61d70485aa73197f03
schaui6 <sanderfer.chau@gmail.com>
2016-01-19 18:41:07 -0800

    GPS

---
+++ week-7/eloquent.js
@@ -0,0 +1,26 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+var hi = "hello";
+  console.log(hi);
+
+prompt("What is your favorite food?");
+alert("Hey! That is my favorite too!");
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+//Status API Training Shop Blog About Pricing
