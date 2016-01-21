1e4ec722e0ed45dc439641ccaef2c2f1dcf98caa
schaui6 <sanderfer.chau@gmail.com>
2016-01-20 18:28:09 -0800

    Group work

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,52 @@
+/*
+Release 1: Tests to User Stories (Sydney)
+INSTRUCTIONS:
+Based on the tests, write user stories that describe what the
+code needs to do. User stories take the following format:
+As a user, I want to...
+The user stories should be easily translated into
+pseudocode by the next person in your group. However, a user
+story is NOT pseudocode -- it should describe the experience
+of someone using the function. Words like array or loop or any
+word not known to a non-technical person should not be used. You
+can specify the function name.
+ANSWER:
+As a user, I want to be able to use a command called "sum" to
+add up all the values within a set, whether the set has an even
+number of values in it or an odd number of values in it.
+As a user, I want to be able to use a command called "mean" to
+find the average of all the values within a set, whether the
+set has an even number of values in it or an odd number of values
+in it.
+As a user, I want to be able to use a command called "median" to
+find the median of all the values within a set, whether the set
+has an even number of values in it or an odd number of values in it.
+*/
+
+/*
+Release 2: User Stories to Psuedocode (Sanderfer)
+INSTRUCTIONS:
+Based only on the user stories you received from the first person, pseudocode the logic you would need to implement each story. Your pseudocode should be small steps that can be easily translated into code.
+
+ANSWER:
+User Story 1:
+-Need to define "sum"
+-There needs to be a list of numbers
+-"Sum" needs to add all values in the list.
+
+User Story 2:
+-Need to define "mean"
+-There needs to be a list of numbers
+-"Mean" needs to take in the list of numbers and output the average.
+-Take sum of all numbers
+-Determin the length of the list (How many numbers are there in the list?)
+-Divide the total sum by the amount of numbers there are in the list to find the mean.
+
+User Story 3:
+-Need to define "median"
+-There needs to be a list of numbers.
+-Sort list from smallest to greatest.
+-Determine the length of the list (how many numbers are there in the list?)
+-Divide the length of the list by 2 to determine the half way point.
+-The number at the halfway point is the median.
+*/
